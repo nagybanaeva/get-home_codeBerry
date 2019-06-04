49c8e4efccbdc7dabd50f78f084fa1e693ea2fba
Nagy-Bana Éva <nagybanaeva@gmail.com>
2019-06-04 13:40:30 +0200

    add user's interface

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-console.log('A számológép használatra kész.');
 
-// Amiket ki kell számolnom:
-// fuelToOffload:
-    // Az az üzemanyagmennyiség, amit leeresztve a súlylimit alá kerülünk, de még haza tudunk jutni.
-	// fuelToOffload = tankCapacity - requiredFuel
-// tankCapacity: Az űrsikló üzemanyagtartályának űrtartalma.
-// requiredFuel: Minimális üzemanyag-mennyiség, amivel visszajutunk a Földre.
-    // requiredFuel = totalDistanceToTravel * (averageLitersOverHundred / 100)
 
-function calculateFuelToOffload(totalDistanceToTravel, averageLitersOverHundred, tankCapacity) {
-    // 1. lépés: Számolja ki a szükséges üzemanyag-mennyiséget.
+function calculateFuelToOffload() {
+   var totalDistanceToTravel = prompt('Hány kilométer távolságot kíván megtenni? Adjon meg egy számot!');
+	var averageLitersOverHundred = prompt('Mi az űrhajó átlagos fogyasztása literben? Adjon meg egy számot!');
+	var tankCapacity = prompt('Mi az űrhajó üzemanyagtartályának űrtartalma literben? Adjon meg egy számot!');
+	
 	var requiredFuel = totalDistanceToTravel * (averageLitersOverHundred / 100);
-	console.log('Szükséges üzemanyag-mennyiség: ' + requiredFuel + ' L');
-
-    // 2. lépés: Számolja ki a lecsapolandó üzemanyagot.
 	var fuelToOffload = tankCapacity - requiredFuel;
-	console.log('Leeresztendő üzemanyag: ' + fuelToOffload + ' L');
+	
+	alert('Az utazáshoz szükséges üzemanyag-mennyiség: ' + requiredFuel + ' L\n' + 'Leadandó üzemanyag-mennyiség: ' + fuelToOffload + ' L');;
 }
+
+calculateFuelToOffload();
